817ca28eab0bbceeed64b88561ffead01503be74
msrecec <msrecec@3plus.hr>
2024-08-19 21:12:32 +0200

    added post, put and delete to movies and also added nodemon

---
+++ data/data.js
@@ -0,0 +1,8 @@
+const movies = [{id: 1, name: "Matrix", author: "nebitno"},
+    {id: 2, name: "LOTR", author: "Peter Jackson"},
+    {id: 3, name: "Godfather", author: "Francis Coppola"}
+]
+// movies.push({id: 1, name: "Matrix", author: "nebitno"})
+// movies.push({id: 2, name: "LOTR", author: "Peter Jackson"})
+
+module.exports = movies;
--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var movies = require('../data/data')
 
 /* GET movies listing. */
-router.get('/', function(req, res, next) {
-    const movies = []
-    movies.push({id: 1, name: "Matrix", author: "nebitno"})
-    movies.push({id: 2, name: "LOTR", author: "Peter Jackson"})
+router.get('/', function (req, res, next) {
+    res.json(movies)
+});
+
+/* POST movies create. */
+router.post('/', function (req, res, next) {
+    movies.push(req.body)
+    res.json(movies)
+});
+
+/* PUT movies update. */
+router.put('/:id', function (req, res, next) {
+    for (let i = 0; i < movies.length; ++i) {
+        const movie = movies[i]
+        if (movie.id === +req.params.id) {
+            movies[i].name = req.body.name
+            movies[i].author = req.body.author
+            res.json(movies)
+            return
+        }
+    }
+    res.json({success: false, message: `can not find film with id ${req.body.id}`})
+});
+
+/* DELETE movies create. */
+router.delete('/:id', function (req, res, next) {
+    for (let i = 0; i < movies.length; ++i) {
+        const movie = movies[i]
+        if (movie.id === +req.params.id) {
+            movies.splice(i, 1);
+            res.json(movies)
+            return
+        }
+    }
     res.json(movies)
 });
 
